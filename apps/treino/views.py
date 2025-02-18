bd5fecec855e4d3d0c078319fff567bb9eb6e4af
gm-costa <gmaia_01@hotmail.com>
2025-02-18 08:17:05 -0300

    Adiciona view update_aluno

--- apps/treino/views.py
+++ apps/treino/views.py
@@ -1,4 +1,6 @@
+from datetime import date
 from typing import List
+from django.shortcuts import get_object_or_404
 from ninja import Router
 from ninja.errors import HttpError
 from .schemas import AlunoSchema
@@ -30,3 +32,23 @@ def criar_aluno(request, aluno_schema: AlunoSchema):
 def listar_alunos(request):
     alunos = Aluno.objects.all()
     return alunos
+
+@treino_router.put("alunos/{aluno_id}/", response={200: str})
+def update_aluno(request, aluno_id: int, aluno_data: AlunoSchema):
+
+    aluno = get_object_or_404(Aluno, id=aluno_id)
+    
+    idade = date.today() - aluno.data_nascimento
+
+    if int(idade.days/365) < 18 and aluno_data.dict()['faixa'] in ('A', 'R', 'M', 'P'):
+        raise HttpError(400, f'Menor de 18 anos não pode ser graduado para a faixa informada.')
+
+    for key, value in aluno_data.dict().items():
+        if value:
+            setattr(aluno, key, value)
+
+    try:
+        aluno.save()
+        return 200, 'Atualização realizada com sucesso.'
+    except Exception as e:
+        HttpError(400, f'Erro: {e}.')
