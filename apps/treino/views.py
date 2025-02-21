44c9b04933b5677d1f5db29fb4d7216a2cc8b438
gm-costa <gmaia_01@hotmail.com>
2025-02-20 21:37:59 -0300

    Modifica retornos das views

--- apps/treino/views.py
+++ apps/treino/views.py
@@ -25,7 +25,7 @@ def criar_aluno(request, aluno_schema: AlunoSchema):
     
     try:
         aluno.save()
-        return 200, f'O aluno {aluno.nome} foi cadastrado com sucesso.'
+        return 200, f'{aluno.nome} foi cadastrado com sucesso.'
     except Exception as e:
         raise HttpError(400, f'Erro: {e}.')
 
@@ -61,42 +61,59 @@ def update_aluno(request, aluno_id: int, aluno_data: AlunoSchema):
     except Exception as e:
         return 400, f'Erro: {e}.'
 
-@treino_router.post('aula-realizada/', response={200: str})
+@treino_router.post('aula-realizada/', response={200: str, 400:str})
 def aula_realizada(request, aula_realizada: AulaRealizadaSchema):
     qtd, email_aluno = aula_realizada.dict().values()
 
     if qtd < 1:
-        raise HttpError(400, 'Quantidade de aulas deve ser maior que zero (0).')
+        return 400, 'Quantidade de aulas deve ser maior que zero (0).'
     
     if not email_aluno:
-        raise HttpError(400, 'E-mail não informado !')
+        return 400, 'E-mail não informado !'
     
     try:
         aluno = Aluno.objects.get(email=email_aluno)
     except Aluno.DoesNotExist:
-        return 200, f'Não existe aluno com o e-mail {email_aluno} !'
+        return 400, f'Não existe aluno com o e-mail {email_aluno} !'
 
     aulas = [AulasConcluidas(aluno=aluno, faixa_atual=aluno.faixa) for _ in range(qtd)]
     
-    AulasConcluidas.objects.bulk_create(aulas)
+    try:
+        AulasConcluidas.objects.bulk_create(aulas)
+        return 200, f'Aula(s) marcada(s) como realizada para {aluno.nome}.'
+    except Exception as ex:
+        return 400, f'Erro: {ex}.'
 
-    return 200, f'Aula(s) marcada(s) como realizada para o aluno {aluno.nome.title()}.'
-
-@treino_router.get('progresso-aluno/', response={200: ProgressoAlunoSchema})
+@treino_router.get('progresso-aluno/', response={200: ProgressoAlunoSchema, 400: str})
 def progresso_aluno(request, email_aluno: str):
-
-    aluno = Aluno.objects.get(email=email_aluno)
+    if not email_aluno:
+        return 400, 'E-mail não informado !'
     
-    faixa_atual = aluno.get_faixa_display()
+    try:
+        aluno = Aluno.objects.get(email=email_aluno)
+        
+        faixa_atual = aluno.get_faixa_display()
+        
+        nivel_faixa = order_faixas.get(faixa_atual, 0)
     
-    nivel_faixa = order_faixas.get(faixa_atual, 0)
-  
-    total_aulas_proxima_faixa = calcular_qtd_aulas_evoluir_de_faixa(nivel_faixa)
+        total_aulas_proxima_faixa = calcular_qtd_aulas_evoluir_de_faixa(nivel_faixa)
 
-    total_aulas_concluidas_faixa = AulasConcluidas.objects.filter(aluno=aluno, faixa_atual=aluno.faixa).count()
+        total_aulas_concluidas_faixa = AulasConcluidas.objects.filter(aluno=aluno, faixa_atual=aluno.faixa).count()
 
-    aulas_faltantes = max(total_aulas_proxima_faixa - total_aulas_concluidas_faixa, 0)
+        aulas_faltantes = max(total_aulas_proxima_faixa - total_aulas_concluidas_faixa, 0)
 
+        return {
+            'nome': aluno.nome.title(),
+            'email': aluno.email,
+            'faixa': aluno.get_faixa_display(),
+            'total_aulas_proxima_faixa': total_aulas_proxima_faixa,
+            'total_aulas_concluidas_faixa_atual': total_aulas_concluidas_faixa,
+            'total_aulas_faltantes_proxima_faixa': aulas_faltantes,
+        }
+
+    except Aluno.DoesNotExist:
+        return 400, f'Não existe aluno com o e-mail {email_aluno} !'
+    
 @treino_router.get('alunos/busca/', response={200: UpdateAlunoSchema, 400: str})
 def busca_aluno(request, email_aluno: str):
     if not email_aluno:
