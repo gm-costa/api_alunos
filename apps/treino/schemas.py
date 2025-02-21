b554d76de14ec7ca27ac84409c89e9324b7bfafd
gm-costa <gmaia_01@hotmail.com>
2025-02-20 21:27:15 -0300

    Adiciona UpdateAlunoSchema

--- apps/treino/schemas.py
+++ apps/treino/schemas.py
@@ -7,13 +7,18 @@ class AlunoSchema(ModelSchema):
         model = Aluno
         fields = ['nome', 'email', 'data_nascimento', 'faixa']
 
+class UpdateAlunoSchema(ModelSchema):
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
 class AulaRealizadaSchema(Schema):
     qtd: Optional[int] = 1
     email_aluno: str
 
 class ProgressoAlunoSchema(Schema):
-    email: str
     nome: str
+    email: str
     faixa: str
     total_aulas_proxima_faixa: int
     total_aulas_concluidas_faixa_atual: int
